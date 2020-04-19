33f371c160600b68d6e7afedd537bd1ed69f8fa0
sameer saurav <sameer.saurav@dailyhunt.in>
2020-04-19 12:16:00 +0530

    restart on cpu usage done

--- app.js
+++ app.js
@@ -10,16 +10,20 @@ var cors = require('cors');
 var app = express();
 const mongoose = require('mongoose');
 
-//var usage       = require('usage');
+var osu = require('async-os-utils')
+var cpu = osu.cpu
 
-mongoose.connect("mongodb+srv://sharad:sharad123@sharad-wezb2.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true ,  useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://sharad:sharad123@sharad-wezb2.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,41 +34,39 @@ app.use("/message", messageRoute)
 var userService = require("./services/userService");
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-CHECK_CPU_USAGE_INTERVAL    = 1000*5; // every minute
-HIGH_CPU_USAGE_LIMIT        = 70; // percentage
+CHECK_CPU_USAGE_INTERVAL = 1000 * 5; // every minute
+HIGH_CPU_USAGE_LIMIT = 70; // percentage
 
-// autoRestart = setInterval(function()
-// {
-//   usage.lookup(process.pid, function(err, result)
-//   {
-//     if(!err)
-//     {
-//       if(result.cpu > HIGH_CPU_USAGE_LIMIT)
-//       {
-//         // log
-//         console.log('restart due to high cpu usage');
-//
-//         // restart because forever will respawn your process
-//         process.exit();
-//       }
-//     }
-//   });
-// }, CHECK_CPU_USAGE_INTERVAL);
-//
-// autoRestart()
+
+autoRestart = setInterval(function () {
+    cpu.usage()
+        .then(cpuPercentage => {
+            console.log(cpuPercentage)
+            if (cpuPercentage > HIGH_CPU_USAGE_LIMIT) {
+                    // log
+                    console.log('restart due to high cpu usage');
+
+                    // restart because forever will respawn your process
+                    process.exit();
+                }
+
+        })
+}, CHECK_CPU_USAGE_INTERVAL);
+
+
 module.exports = app;
