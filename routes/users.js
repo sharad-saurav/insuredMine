69ff3de0c1329c09ce0341935db01e9c88b595ad
sameer saurav <sameer.saurav@dailyhunt.in>
2020-04-20 00:49:38 +0530

    kxjhckjdxnc

--- routes/users.js
+++ routes/users.js
@@ -11,15 +11,9 @@ require('../models/policyCarrier');
 require('../models/policyCategory');
 require('../models/policyInfo');
 require('../models/userAccount');
-var Promise = require('promise');
 
-const userService = require('../services/userService');
-const User = mongoose.model('User');
-const Agent = mongoose.model('Agent');
-const PolicyCategory = mongoose.model('PolicyCategory');
-const PolicyCarrier = mongoose.model('PolicyCarrier');
-const UserAccount = mongoose.model('UserAccount');
-const PolicyInfo = mongoose.model('PolicyInfo');
+const workerPool = require('../services/workerPool');
+
 var fileupload = require('express-fileupload');
 router.use(fileupload());
 
@@ -34,125 +28,14 @@ router.post('/uploadCsv', function(req, res) {
       const csv=require('csvtojson')
       csv()
       .fromFile(path)
-      .then((jsonObj)=>{
-        async.each(jsonObj, function (data, callback) {
-
-          var promise = new Promise(function(resolve, reject){
-            var agent = new Agent({agentName: data.agent});
-            var userObj = {};
-            userObj['firstname'] = data.firstname;
-            userObj['dob'] = data.dob;
-            userObj['address'] = data.address;
-            userObj['phone'] = data.phone;
-            userObj['state'] = data.state;
-            userObj['gender'] = data.gender;
-            userObj['email'] = data.email;
-            userObj['userType'] = data.userType;
-            userObj['firstname'] = data.firstname;
-            userObj['firstname'] = data.firstname;
-            userObj['firstname'] = data.firstname;
-            userObj['firstname'] = data.firstname;
-            userObj['firstname'] = data.firstname;
-            userObj['firstname'] = data.firstname;
-            userObj['firstname'] = data.firstname;
-            userObj['firstname'] = data.firstname;
-
-            var policyInfoObj = {};
-            policyInfoObj['policyNumber'] = data.policy_number;
-            policyInfoObj['policyStartDate'] = data.policy_start_date;
-            policyInfoObj['policyEndDate'] = data.policy_end_date;
-            policyInfoObj['policyCategory'] = data.category_name;
-
-            var user = new User(userObj);
-            
-            var policyCategory = new PolicyCategory({categoryName:data.category_name});
-
-            var policyCarrier = new PolicyCarrier({companyName: data.company_name});
-
-            var userAccount = new UserAccount({accountName: data.account_name});
-
-            var globalObj = {};
-            var error = {"value": "", "status": false};
-            
-            agent.save(function(err, data){
-              if(err){
-                error.status = true;
-                error.value = err;
-              }else{
-                globalObj['agentId'] = data._id;
-                policyInfoObj.collectionId = data._id;
-              }
-            }) 
-
-            policyCarrier.save(function(err, data){
-              if(err){
-                error.status = true;
-                error.value = err;
-              }else{
-                globalObj['policyCarrierId'] = data._id;
-                policyInfoObj.companyCollectionId = data._id;
-              }
-            }) 
-
-            policyCategory.save(function(err, data){
-              if(err){
-                error.status = true;
-                error.value = err;
-              }else{
-                globalObj['policyCategoryId'] = data._id;
-              }
-            }) 
-
-            user.save(function(err, userData){
-              if(err){
-                error.status = true;
-                error.value = err;
-              } else {
-                userAccount.userId = userData._id;
-                console.log("userData._id--------",userData._id);
-                policyInfoObj.userId = userData._id;
-                userAccount.save(function(err, userAccountData){
-                  if(err){
-                    error.status = true;
-                    error.value = err;
-                  } else {
-                    globalObj['userAccountId'] = userAccountData._id;
-                  }
-                })
-              }
-            }) 
-            if(error.status){
-              console.log("error.value------",error.value)
-              reject(error.value);
-            }else{
-
-              resolve(policyInfoObj)
-            }
-          }) 
-          promise.then(function (temp){
-            console.log("policyInfoObj----------",temp);
-            var policyInfo = new PolicyInfo(temp);
-            policyInfo.save(function(err, data){
-              if(err){
-                callback(err);
-              }else{
-                console.log('data-----',data);
-                callback()
-              }
-            }) 
-          }).catch(function(err){
-            callback(err);
-          })
-        }, function (err) {
-          if (err) {
-            console.log("err6-----------------",err);
-            res.status(400).send(err);
-          } else {
-            res.status(200).send("success");
-          }
-        })
+      .then((parsedData)=>{
+        workerPool.executeTask(parsedData)
+            .then(() => {
+              res.send({});
+            })
       })
     }
   })
 })
+
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -17,39 +17,65 @@ Promise.promisifyAll(mongoose);
  * @returns
  */
 
-    function registerUser(data){
-        return new Promise(function (resolve, reject) {
-            User.find({}).exec().then(users =>{
-                if(users.length != 0){
-                    data.auth = "member"
-                }else{
-                    data.auth = "admin"
-                }
-                var user = new User();
-                var userRoles = new UserRoles();
-                user.name = data.name;
-                user.password = data.password;
-                user.email = data.email;
-                user.save(function(err, savedData){
-                    if(err){
-                        reject(err);
-                    }else{
-                        userRoles.auth = data.auth;
-                        userRoles.userId = savedData._id
-                        userRoles.save(function(err){
-                            if(err) {
-                                reject(err);
-                            } else {
-                                resolve(data);
-                            }
-                        })
-                    }
-                })
-            })
-        })
-    };
+function saveData(data) {
+    var agent = new Agent({agentName: data.agent});
+    var agentPromise = agent.save();
 
-    var toExport = {
-        registerUser
-    };
-    module.exports = toExport;
+
+    var userObj = {};
+    userObj['firstname'] = data.firstname;
+    userObj['dob'] = data.dob;
+    userObj['address'] = data.address;
+    userObj['phone'] = data.phone;
+    userObj['state'] = data.state;
+    userObj['gender'] = data.gender;
+    userObj['email'] = data.email;
+    userObj['userType'] = data.userType;
+    userObj['firstname'] = data.firstname;
+    userObj['firstname'] = data.firstname;
+    userObj['firstname'] = data.firstname;
+    userObj['firstname'] = data.firstname;
+    userObj['firstname'] = data.firstname;
+    userObj['firstname'] = data.firstname;
+    userObj['firstname'] = data.firstname;
+    userObj['firstname'] = data.firstname;
+
+    var user = new User(userObj);
+    var userPromise = user.save();
+
+    var policyCategory = new PolicyCategory({categoryName: data.category_name});
+    var policyCategoryPromise = policyCategory.save();
+
+    var policyCarrier = new PolicyCarrier({companyName: data.company_name});
+    var policyCarrrierPromise = policyCarrier.save();
+
+
+    Promise.all([policyCarrrierPromise, policyCategoryPromise, userPromise, agentPromise])
+        .then((values) => {
+            var userAccount = new UserAccount({accountName: data.account_name});
+            userAccount.userId = values[2]._id;
+            var userAccountPromise = userAccount.save();
+
+            var policyInfoObj = {};
+            policyInfoObj['policyNumber'] = data.policy_number;
+            policyInfoObj['policyStartDate'] = data.policy_start_date;
+            policyInfoObj['policyEndDate'] = data.policy_end_date;
+            policyInfoObj['policyCategory'] = data.category_name;
+            policyInfoObj.policyCategory = values[1]._id;
+            policyInfoObj.collectionId = values[3]._id;
+            policyInfoObj.companyCollectionId = values[0]._id;
+            policyInfoObj.userId = values[2]._id;
+
+            var policyInfoPromise = new PolicyInfo(policyInfoObj).save();
+
+            Promise.all([userAccountPromise, policyInfoPromise])
+                .then((values) => {
+                    console.log("all done")
+                })
+        })
+}
+
+var toExport = {
+    saveData
+}
+module.exports = toExport;
