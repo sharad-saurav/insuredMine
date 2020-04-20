923186a6a21069f514e41a7d15505cf44cf40fcf
sameer saurav <sameer.saurav@dailyhunt.in>
2020-04-20 13:34:54 +0530

    pata nhi ka fix kiye hain

--- services/userService.js
+++ services/userService.js
@@ -9,85 +9,88 @@
  */
 
 function saveData(data) {
+    return new Promise((resolve, reject) => {
+        var mongoose = require('mongoose');
+        require('../../../models/userRoles');
 
-    var mongoose = require('mongoose');
-    require('../../../models/userRoles');
+        require('../../../models/agent');
+        require('../../../models/user');
+        require('../../../models/message');
+        require('../../../models/policyCarrier');
+        require('../../../models/policyCategory');
+        require('../../../models/policyInfo');
+        require('../../../models/userAccount');
 
-    require('../../../models/agent');
-    require('../../../models/user');
-    require('../../../models/message');
-    require('../../../models/policyCarrier');
-    require('../../../models/policyCategory');
-    require('../../../models/policyInfo');
-    require('../../../models/userAccount');
+        const Agent = mongoose.model('Agent');
+        const User = mongoose.model('User');
+        const PolicyCarrier = mongoose.model('PolicyCarrier');
+        const PolicyCategory = mongoose.model('PolicyCategory');
+        const PolicyInfo = mongoose.model('PolicyInfo');
+        const UserAccount = mongoose.model('UserAccount');
 
-    const Agent = mongoose.model('Agent');
-    const User = mongoose.model('User');
-    const PolicyCarrier = mongoose.model('PolicyCarrier');
-    const PolicyCategory = mongoose.model('PolicyCategory');
-    const PolicyInfo = mongoose.model('PolicyInfo');
-    const UserAccount = mongoose.model('UserAccount');
+        let execArray = [];
 
-    let execArray = [];
+        var agent = new Agent({agentName: data.agent});
+        execArray.push(agent.save());
 
-    var agent = new Agent;
-    execArray.push(agent.save());
-    
-    var userObj = {};
-    userObj['firstname'] = data.firstname;
-    userObj['dob'] = data.dob;
-    userObj['address'] = data.address;
-    userObj['phone'] = data.phone;
-    userObj['state'] = data.state;
-    userObj['gender'] = data.gender;
-    userObj['email'] = data.email;
-    userObj['userType'] = data.userType;
-    userObj['firstname'] = data.firstname;
-    userObj['firstname'] = data.firstname;
-    userObj['firstname'] = data.firstname;
-    userObj['firstname'] = data.firstname;
-    userObj['firstname'] = data.firstname;
-    userObj['firstname'] = data.firstname;
-    userObj['firstname'] = data.firstname;
-    userObj['firstname'] = data.firstname;
+        var userObj = {};
+        userObj['firstname'] = data.firstname;
+        userObj['dob'] = data.dob;
+        userObj['address'] = data.address;
+        userObj['phone'] = data.phone;
+        userObj['state'] = data.state;
+        userObj['gender'] = data.gender;
+        userObj['email'] = data.email;
+        userObj['userType'] = data.userType;
+        userObj['firstname'] = data.firstname;
+        userObj['firstname'] = data.firstname;
+        userObj['firstname'] = data.firstname;
+        userObj['firstname'] = data.firstname;
+        userObj['firstname'] = data.firstname;
+        userObj['firstname'] = data.firstname;
+        userObj['firstname'] = data.firstname;
+        userObj['firstname'] = data.firstname;
 
-    var user = new User(userObj);
-    execArray.push(user.save());
-
-    
-
-    var policyCategory = new PolicyCategory({categoryName: data.category_name});
-    execArray.push(policyCategory.save());
-
-    var policyCarrier = new PolicyCarrier({companyName: data.company_name});
-    execArray.push(policyCarrier.save());
+        var user = new User(userObj);
+        execArray.push(user.save());
 
 
-    Promise.all(execArray)
-        .then((values) => {
 
-            let dependentCollectionsExecArray = [];
-            var userAccount = new UserAccount({accountName: data.account_name});
-            userAccount.userId = values[2]._id;
-            dependentCollectionsExecArray.push(userAccount.save());
+        var policyCategory = new PolicyCategory({categoryName: data.category_name});
+        execArray.push(policyCategory.save());
 
-            var policyInfoObj = {};
-            policyInfoObj['policyNumber'] = data.policy_number;
-            policyInfoObj['policyStartDate'] = data.policy_start_date;
-            policyInfoObj['policyEndDate'] = data.policy_end_date;
-            policyInfoObj['policyCategory'] = data.category_name;
-            policyInfoObj.policyCategory = values[1]._id;
-            policyInfoObj.collectionId = values[3]._id;
-            policyInfoObj.companyCollectionId = values[0]._id;
-            policyInfoObj.userId = values[2]._id;
+        var policyCarrier = new PolicyCarrier({companyName: data.company_name});
+        execArray.push(policyCarrier.save());
 
-            dependentCollectionsExecArray.push(new PolicyInfo(policyInfoObj).save());
 
-            Promise.all(dependentCollectionsExecArray)
-                .then((values) => {
-                    console.log("all done")
-                })
-        })
+        Promise.all(execArray)
+            .then((values) => {
+
+                console.log("first done")
+                let dependentCollectionsExecArray = [];
+                var userAccount = new UserAccount({accountName: data.account_name});
+                userAccount.userId = values[2]._id;
+                dependentCollectionsExecArray.push(userAccount.save());
+
+                var policyInfoObj = {};
+                policyInfoObj['policyNumber'] = data.policy_number;
+                policyInfoObj['policyStartDate'] = data.policy_start_date;
+                policyInfoObj['policyEndDate'] = data.policy_end_date;
+                policyInfoObj['policyCategory'] = data.category_name;
+                policyInfoObj.policyCategory = values[1]._id;
+                policyInfoObj.collectionId = values[3]._id;
+                policyInfoObj.companyCollectionId = values[0]._id;
+                policyInfoObj.userId = values[2]._id;
+
+                dependentCollectionsExecArray.push(new PolicyInfo(policyInfoObj).save());
+
+                Promise.all(dependentCollectionsExecArray)
+                    .then((values) => {
+                        console.log("all done")
+                    })
+            })
+    })
+
 }
 
 var toExport = {
